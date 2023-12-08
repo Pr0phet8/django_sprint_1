3c9597bba6a2cc2487a80209e8b82e47efa5e359
Даниил Никишин <daniel785@yandex.ru>
2023-12-09 00:36:09 +0500

    Homework

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog.apps.BlogConfig',
+    'pages.apps.PagesConfig',
+
 ]
 
 MIDDLEWARE = [
@@ -51,10 +54,16 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'blogicum.urls'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+TEMPLATES_DIR = BASE_DIR / 'templates'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
